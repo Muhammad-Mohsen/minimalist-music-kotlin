66658c7880ff3d30fb9c83e36701bac6d57966e0
Muhammad Mohsen <muhammadaeh@gmail.com>
2023-06-11 01:19:58 +0300

    Replaced Dexter permission lib with a native solution

--- app/src/main/java/mohsen/muhammad/minimalist/app/main/MainFragment.kt
+++ app/src/main/java/mohsen/muhammad/minimalist/app/main/MainFragment.kt
@@ -3,19 +3,16 @@ package mohsen.muhammad.minimalist.app.main
 import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.karumi.dexter.Dexter
-import com.karumi.dexter.PermissionToken
-import com.karumi.dexter.listener.PermissionDeniedResponse
-import com.karumi.dexter.listener.PermissionGrantedResponse
-import com.karumi.dexter.listener.PermissionRequest
-import com.karumi.dexter.listener.single.PermissionListener
 import mohsen.muhammad.minimalist.app.breadcrumb.BreadcrumbManager
 import mohsen.muhammad.minimalist.app.explorer.ExplorerManager
 import mohsen.muhammad.minimalist.app.player.PlaybackManager
@@ -29,6 +26,7 @@ import mohsen.muhammad.minimalist.databinding.MainFragmentBinding
 class MainFragment : Fragment() {
 
 	private lateinit var binding: MainFragmentBinding
+	private lateinit var permissionRequest: ActivityResultLauncher<String>
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -37,6 +35,19 @@ class MainFragment : Fragment() {
 		val preferences = requireActivity().getSharedPreferences(Const.MINIMALIST_SHARED_PREFERENCES, Context.MODE_PRIVATE)
 		State.initialize(preferences)
 
+		// storage permission
+		permissionRequest = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+			if (isGranted) {
+				initialize()
+			} else {
+				binding.layoutPermission.root.visibility = View.VISIBLE
+				binding.layoutPermission.buttonGrantPermission.setOnClickListener {
+					initialize()
+				}
+			}
+		}
+
+		// back button
 		requireActivity().onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
 			override fun handleOnBackPressed() {
 				onBackPressed()
@@ -47,49 +58,41 @@ class MainFragment : Fragment() {
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
 		binding = MainFragmentBinding.inflate(inflater, container, false)
 		initialize()
+
 		return binding.root
 	}
 
 	private fun initialize() {
+		when {
+			ContextCompat.checkSelfPermission(requireContext(), PERMISSION) == PackageManager.PERMISSION_GRANTED -> {
+				binding.layoutPermission.root.visibility = View.GONE
 
-		// ask for permission
-		Dexter.withContext(requireActivity())
-			.withPermission(PERMISSION)
-			.withListener(object : PermissionListener {
+				// service
+				val playerIntent = Intent(requireActivity(), PlaybackManager::class.java)
+				ContextCompat.startForegroundService(requireActivity(), playerIntent)
 
-				override fun onPermissionGranted(response: PermissionGrantedResponse?) {
-					binding.layoutPermission.root.visibility = View.GONE
+				// breadcrumbs
+				val breadcrumbManager = BreadcrumbManager(binding)
+				breadcrumbManager.initialize()
 
-					// service
-					val playerIntent = Intent(requireActivity(), PlaybackManager::class.java)
-					ContextCompat.startForegroundService(requireActivity(), playerIntent)
+				// explorer
+				val explorerManager = ExplorerManager(binding.recyclerViewExplorer)
+				explorerManager.initialize()
 
-					// breadcrumbs
-					val breadcrumbManager = BreadcrumbManager(binding)
-					breadcrumbManager.initialize()
+				// controls
+				val playerControlsManager = PlayerControlsManager2(binding)
+				playerControlsManager.initialize()
 
-					// explorer
-					val explorerManager = ExplorerManager(binding.recyclerViewExplorer)
-					explorerManager.initialize()
-
-					// controls
-					val playerControlsManager = PlayerControlsManager2(binding)
-					playerControlsManager.initialize()
-
-					// after initializing everything, restore the state - at this point, the Playback service isn't started yet, so it hasn't yet registered to the event bus!
-					if (State.Track.isInitialized) EventBus.send(SystemEvent(EventSource.FRAGMENT, EventType.METADATA_UPDATE))
-				}
-				override fun onPermissionRationaleShouldBeShown(permission: PermissionRequest?, token: PermissionToken?) { token?.continuePermissionRequest() }
-
-				// show permission layout
-				override fun onPermissionDenied(response: PermissionDeniedResponse?) {
-					binding.layoutPermission.root.visibility = View.VISIBLE
-					binding.layoutPermission.buttonGrantPermission.setOnClickListener {
-						initialize()
-					}
-				}
-
-			}).check()
+				// after initializing everything, restore the state - at this point, the Playback service isn't started yet, so it hasn't yet registered to the event bus!
+				if (State.Track.isInitialized) EventBus.send(SystemEvent(EventSource.FRAGMENT, EventType.METADATA_UPDATE))
+			}
+			shouldShowRequestPermissionRationale(PERMISSION) -> {
+				permissionRequest.launch(PERMISSION)
+			}
+			else -> {
+				permissionRequest.launch(PERMISSION)
+			}
+		}
 	}
 
 	private fun onBackPressed() {

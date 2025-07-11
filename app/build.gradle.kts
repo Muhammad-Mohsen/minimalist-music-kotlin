be8ce1eb8e41e9f651ab479dbf32e0b2facc8907
Muhammad Mohsen <muhammadaeh@gmail.com>
2025-07-11 15:29:47 +0300

    chore (gradle): enable minification in release builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
 			applicationIdSuffix = ".debug"
 		}
 		release {
-			isMinifyEnabled = false
+			isMinifyEnabled = true
 			proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
 
 			ndk {
